a8c67f0a6d2a0ee37007ed72a5d77f9e241ef3e6
MOON CAT <mhmohamed586@gmail.com>
2024-08-02 08:39:13 +0100

    last update

--- src/app/module/smartphone.ts
+++ src/app/module/smartphone.ts
@@ -4,7 +4,8 @@ export class smartphone{
         public number:number,
         public imgURl: string,
         public imgs:string[],
-        public desc:string){
+        public desc:string,
+        public fabrication:string){
             this.id=crypto.randomUUID().substring(0.8);
         }
 
--- src/app/phone-list/phone-list.component.ts
+++ src/app/phone-list/phone-list.component.ts
@@ -3,11 +3,12 @@ import { MarComponent } from '../mar/mar.component';
 import { smartphone } from '../module/smartphone';
 import { CommonModule } from '@angular/common';
 import { servicePH } from '../services/servicePH';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-phone-list',
   standalone: true,
-  imports: [MarComponent,CommonModule],
+  imports: [MarComponent,CommonModule,FormsModule],
   templateUrl: './phone-list.component.html',
   styleUrl: './phone-list.component.scss'
 })
@@ -18,12 +19,41 @@ export class PhoneListComponent implements OnInit{
   }
 
   smartphones!:smartphone[];
+  filteredSmartphones!: smartphone[];
+  fabrications: string[] = [];
+  selectedFabrication: string = '';
+
+
+
 
   ngOnInit(): void {
     this.smartphones = this.phoneS.getphones();
+    this.filteredSmartphones = this.smartphones;
+    this.fabrications = this.getUniqueFabrications();
   }
+
+
+
+
+
+
   trackBySmartphoneName(index: number, phone: smartphone): string {
     return phone.id; // Use the 'name' as a unique identifier
+  }
+
+  onFabricationChange(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const fabrication = selectElement.value;
+    this.selectedFabrication = fabrication;
+    this.filteredSmartphones = fabrication ?
+      this.smartphones.filter(phone => phone.fabrication === fabrication) :
+      this.smartphones;
+  }
+
+
+  getUniqueFabrications(): string[] {
+    
+    return [...new Set(this.smartphones.map(phone => phone.fabrication))];
   }
 
 }
